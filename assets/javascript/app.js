00c528ffbfeb70091ef5b25554520d425758c331
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-02-23 19:49:08 -0800

    Added gameBoard object and question randomize method

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,30 @@
 var intervalID;
 var qClock = 10;
 
+var q1 = {
+  question:
+    "<span class='alt-code-blue' id='question-label'>Question: </span><span class='alt-code'>&lt;a&gt;</span> and<span class='alt-code'>&lt;/a&gt;</span> are the tags used for?",
+  correctAnswer: "Adding links to your page",
+  possibleAnswers: [
+    "Adding links to your page",
+    "Aligning Text",
+    "Adding images",
+    "Adding audio"
+  ],
+  randomize: function() {
+    this.possibleAnswers.sort(function(a, b) {
+      return 0.5 - Math.random();
+    });
+  }
+};
+
+var gameBoard = {
+  correctAnswers: 0,
+  wrongAnswers: 0,
+  unanswered: 0,
+  questions: [q1]
+};
+
 function stop() {
   clearInterval(intervalID);
 }
@@ -31,8 +55,11 @@ function startGame() {
   startTimer();
 }
 
+function randomizeGame() {}
+
 $(window).on("load", function() {
   $(".start").on("click", function() {
     startGame();
   });
+  randomizeGame();
 });
